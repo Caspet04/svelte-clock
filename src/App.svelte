17a50b8a78bc912043b27e6bad5fc82a834ef038
Caspet04 <caspet@edu.umea.se>
2022-02-03 13:15:04 +0100

    Fixed clock config overlay to work with mobile

--- src/App.svelte
+++ src/App.svelte
@@ -85,14 +85,14 @@
 					<div id="clock-{i}">
 						<h2>{ ["Left Clock", "Center Clock", "Right Clock"][i-1] }</h2>
 						<div class="clock-controls">
-							<span style="grid-row: 1; grid-column: 1">Timezone: </span>
-							<select style="grid-row: 1; grid-column: 2 / 3" bind:value={selectedTimezone[i-1]} name="timezone" id="clock-{i}-timezone">
+							<span class="timezone-label" >Timezone: </span>
+							<select class="timezone-selector" bind:value={selectedTimezone[i-1]} name="timezone" id="clock-{i}-timezone">
 								{#each timezones as timezone}
 									<option value="{timezone.value}">{timezone.label}</option>
 								{/each}
 							</select>
-							<label style="grid-row: 2; grid-column: 1">Alarm: <input type="checkbox" bind:value={clocks[i-1].alarmIsActive}></label>
-							<input style="grid-row: 2; grid-column: 2; width: 100px;" type="time" bind:value={clocks[i-1].alarmTimeAsString}>
+							<label class="alarm-check">Alarm: <input type="checkbox" bind:value={clocks[i-1].alarmIsActive}></label>
+							<input class="alarm-time" type="time" bind:value={clocks[i-1].alarmTimeAsString}>
 						</div>
 					</div>
 				{/each}
@@ -105,133 +105,129 @@
 			<li><button on:click="{() => { clockConfigOverlayHidden = false; }}">Config Clocks</button></li>
 		</ul>
 	</div>
-	<div id="content">
-		<div id="clock-container">
-			<!-- "Normal" analog style clock -->
-			<div class="clock" id="analog">
-				<svg viewBox='-50 -50 100 100'>
-					<!-- Clock markers, alternating larger and smaller every 5th marker -->
-					{#each range(0, 60, 1) as marker}
-						<line class='marker-{marker % 5 == 0 ? "large" : "small"}'
-							y1='{45 - (marker % 5 == 0 ? 10 : 5)}'
-							y2='45'
-							transform='rotate({6 * marker})' />
-					{/each}
-					<!-- Clock numbers, they are rotated to place them in a circle and then the text is rotated back -->
-					{#each range(1, 13, 1) as clockNumber}
-						<g transform='rotate({-180+30*clockNumber})'>
-							<text
-								transform='rotate({180-30*clockNumber},0,29)'
-								text-anchor='middle'
-								y='32'>
-								{clockNumber}
-							</text>
-						</g>
-					{/each}
-		
-					<!-- Hour hand -->
-					<line class='hand' id='hour'
-						y1='-5'
-						y2='35'
-						transform='rotate({180 + 30 * clocks[0].hour})' />
-		
-					<!-- Minute hand -->
-					<line class='hand' id='minute'
-						y1='-7'
+	<div id="clock-container">
+		<!-- "Normal" analog style clock -->
+		<div class="clock" id="analog">
+			<svg viewBox='-50 -50 100 100'>
+				<!-- Clock markers, alternating larger and smaller every 5th marker -->
+				{#each range(0, 60, 1) as marker}
+					<line class='marker-{marker % 5 == 0 ? "large" : "small"}'
+						y1='{45 - (marker % 5 == 0 ? 10 : 5)}'
 						y2='45'
-						transform='rotate({180 + 6 * clocks[0].minute})' />
-		
-					<!-- Second hand -->
-					<line class='hand' id='second'
-						y1='-10'
-						y2='45'
-						transform='rotate({180 + 6 * clocks[0].second})' />
-		
-					<!-- Outside circle -->
-					<circle class='clock-outside'
-						cx='0'
-						cy='0'
-						r='45' />
-				</svg>
-				{#if clocks[0].alarmIsTriggered }
-					ALARM
-				{/if}
-			</div>
-			<div class="clock" id="digital">
-				<div class="clock">
-					{#key timeString[0]}
-						<span in:fly="{{y: -20}}">{timeString[0]}</span>
-					{/key}
-					{#key timeString[1]}
-						<span in:fly="{{y: -20}}">{timeString[1]}</span>
-					{/key}
-		
-					:
-		
-					{#key timeString[3]}
-						<span in:fly="{{y: -20}}">{timeString[3]}</span>
-					{/key}
-					{#key timeString[4]}
-						<span in:fly="{{y: -20}}">{timeString[4]}</span>
-					{/key}
-		
-					:
+						transform='rotate({6 * marker})' />
+				{/each}
+				<!-- Clock numbers, they are rotated to place them in a circle and then the text is rotated back -->
+				{#each range(1, 13, 1) as clockNumber}
+					<g transform='rotate({-180+30*clockNumber})'>
+						<text
+							transform='rotate({180-30*clockNumber},0,29)'
+							text-anchor='middle'
+							y='32'>
+							{clockNumber}
+						</text>
+					</g>
+				{/each}
+	
+				<!-- Hour hand -->
+				<line class='hand' id='hour'
+					y1='-5'
+					y2='35'
+					transform='rotate({180 + 30 * clocks[0].hour})' />
+	
+				<!-- Minute hand -->
+				<line class='hand' id='minute'
+					y1='-7'
+					y2='45'
+					transform='rotate({180 + 6 * clocks[0].minute})' />
+	
+				<!-- Second hand -->
+				<line class='hand' id='second'
+					y1='-10'
+					y2='45'
+					transform='rotate({180 + 6 * clocks[0].second})' />
+	
+				<!-- Outside circle -->
+				<circle class='clock-outside'
+					cx='0'
+					cy='0'
+					r='45' />
+			</svg>
+			{#if clocks[0].alarmIsTriggered }
+				ALARM
+			{/if}
+		</div>
+		<div class="clock" id="digital">
+			{#key timeString[0]}
+				<span in:fly="{{y: -20}}">{timeString[0]}</span>
+			{/key}
+			{#key timeString[1]}
+				<span in:fly="{{y: -20}}">{timeString[1]}</span>
+			{/key}
 
-					{#key timeString[6]}
-						<span in:fly="{{y: -20}}">{timeString[6]}</span>
-					{/key}
-					{#key timeString[7]}
-						<span in:fly="{{y: -20}}">{timeString[7]}</span>
-					{/key}
-				</div>
-			</div>
-			<div class="clock" id="strip">
-				<svg viewBox='0 {(20*stripHoleSize + stripHeightPadding*2)/-2} {stripWidth+2} {20*stripHoleSize + stripHeightPadding*2}' width='100%' height='100%'>
-					{#each range(0, 6, 1) as i}
-						<g transform="translate({1 + stripSidePadding + stripHoleSize*i + Math.floor((i+1)/2)*stripHolePadding + Math.floor(i/2)*stripHoleGap},{-stripHoleSize*stripYOffset[i]-stripHoleSize/2 - stripHeightPadding - 1})">
-							<rect
-								width="{stripHoleSize}"
-								height="{stripHoleSize*stripSizes[i] + stripHeightPadding*2 + 2}"
-								rx="1" ry="1"
-								fill="gray" />
-							{#each range(0, stripSizes[i], 1) as j}
-								<rect
-									width="{stripHoleSize-0.2}"
-									height="{stripHoleSize-0.2}"
-									rx="{stripHoleSize-0.2}"
-									ry="{stripHoleSize-0.2}"
-									y="{stripHoleSize*j + stripHeightPadding*2 + 0.1}"
-									x="0.1"
-									fill="white" />
-								<text
-									text-anchor="middle"
-									x="{stripHoleSize/2}"
-									y="{stripHoleSize * j + stripHeightPadding + stripHoleSize}">{j}</text>
-							{/each}
-						</g>
-					{/each}
-					<mask id="numberHoles">
+			:
+
+			{#key timeString[3]}
+				<span in:fly="{{y: -20}}">{timeString[3]}</span>
+			{/key}
+			{#key timeString[4]}
+				<span in:fly="{{y: -20}}">{timeString[4]}</span>
+			{/key}
+
+			:
+
+			{#key timeString[6]}
+				<span in:fly="{{y: -20}}">{timeString[6]}</span>
+			{/key}
+			{#key timeString[7]}
+				<span in:fly="{{y: -20}}">{timeString[7]}</span>
+			{/key}
+		</div>
+		<div class="clock" id="strip">
+			<svg viewBox='0 {(20*stripHoleSize + stripHeightPadding*2)/-2} {stripWidth+2} {20*stripHoleSize + stripHeightPadding*2}' width='100%' height='100%'>
+				{#each range(0, 6, 1) as i}
+					<g transform="translate({1 + stripSidePadding + stripHoleSize*i + Math.floor((i+1)/2)*stripHolePadding + Math.floor(i/2)*stripHoleGap},{-stripHoleSize*stripYOffset[i]-stripHoleSize/2 - stripHeightPadding - 1})">
 						<rect
-							x="0" y="-50"
-							width="100%" height="100%"
-							fill="white"/>
-		
-						{#each range(0, 6, 1) as i}
+							width="{stripHoleSize}"
+							height="{stripHoleSize*stripSizes[i] + stripHeightPadding*2 + 2}"
+							rx="1" ry="1"
+							fill="gray" />
+						{#each range(0, stripSizes[i], 1) as j}
 							<rect
-								x="{1 + stripSidePadding + stripHoleSize*i + Math.floor((i+1)/2)*stripHolePadding + Math.floor(i/2)*stripHoleGap}"
-								y="{-stripHoleSize / 2}"
-								width="{stripHoleSize}" height="{stripHoleSize}"
-								rx="1" ry="1"
-								fill="black"/>
+								width="{stripHoleSize-0.2}"
+								height="{stripHoleSize-0.2}"
+								rx="{stripHoleSize-0.2}"
+								ry="{stripHoleSize-0.2}"
+								y="{stripHoleSize*j + stripHeightPadding*2 + 0.1}"
+								x="0.1"
+								fill="white" />
+							<text
+								text-anchor="middle"
+								x="{stripHoleSize/2}"
+								y="{stripHoleSize * j + stripHeightPadding + stripHoleSize}">{j}</text>
 						{/each}
-					</mask>
+					</g>
+				{/each}
+				<mask id="numberHoles">
 					<rect
-						x="1" y="{-stripHoleSize/2 - 1}"
-						width="{stripWidth}" height="{stripHoleSize + stripHeightPadding*2}"
-						rx="2" ry="2"
-						mask="url(#numberHoles)" />
-				</svg>
-			</div>
+						x="0" y="-50"
+						width="100%" height="100%"
+						fill="white"/>
+	
+					{#each range(0, 6, 1) as i}
+						<rect
+							x="{1 + stripSidePadding + stripHoleSize*i + Math.floor((i+1)/2)*stripHolePadding + Math.floor(i/2)*stripHoleGap}"
+							y="{-stripHoleSize / 2}"
+							width="{stripHoleSize}" height="{stripHoleSize}"
+							rx="1" ry="1"
+							fill="black"/>
+					{/each}
+				</mask>
+				<rect
+					x="1" y="{-stripHoleSize/2 - 1}"
+					width="{stripWidth}" height="{stripHoleSize + stripHeightPadding*2}"
+					rx="2" ry="2"
+					mask="url(#numberHoles)" />
+			</svg>
 		</div>
 	</div>
 </main>
